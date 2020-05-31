4dbccc2cf384e16517660bb33b00ff9405285c7c
Cathy D'Onofrio <catd825@gmail.com>
2020-05-31 17:07:58 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = "" #create new variable
+passengers.each do |suite, name| #iterate over passenger array - suite & name.  used each vs collect/map since we do not want the key/value pair, just the winner's name.
+  if suite == :suite_a && name.start_with?("A") # if the suite is suite a and name starts with A, then that is the winner
+    winner = name
+  end
+end
+winner
 end
